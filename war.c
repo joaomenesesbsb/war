9d8b3207c934f97c55c19e076094b74a14ef3f9e
Joao Victor <joaovic2012@hotmail.com>
2025-11-24 17:31:34 -0300

    Mostrando dados na tela

--- war.c
+++ war.c
@@ -14,19 +14,35 @@ int main() {
     // Criando struct territorio
     Territorio territorio[5];
 
+    //inserir os dados em cada posicao do vetor
     for(int i = 0; i < 5 ; i++)
     {
         printf("\nTerritorio %d:\n", i + 1);
 
+        //leitor do nome
         printf("Nome: ");
         scanf("%29s", territorio[i].nome);
 
+        //leitor da cor
         printf("Cor: ");
         scanf("%9s", &territorio[i].cor);
 
+        //leitor de tropas
         printf("Tropas: ");
-        scanf("%e", &territorio[i].tropas);
+        scanf("%d", &territorio[i].tropas);
     }
 
+    //mostrar dados
+     printf("\n--- Dados dos Territorios ---\n");
+
+        //ler todos os dados de de cada posicao do vetor
+        for (int i = 0; i < 5; i++) {
+            printf("Territorio %d : -- nome %s -- cor  %s -- %d tropas --\n",
+                i + 1,
+                territorio[i].nome,
+                territorio[i].cor,
+                territorio[i].tropas
+    );
+}
    
 }

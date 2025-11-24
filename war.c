8ba2ae27df64c7e32635775644432b74814ff6d3
Joao Victor <joaovic2012@hotmail.com>
2025-11-24 16:58:48 -0300

    struct criada

---
+++ war.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// Definindo a estrutura territorio
+typedef struct {
+    char nome[30];
+    char cor[10];
+    int tropas;
+} Territorio;
+
+
+int main() {
+
+    // Criando struct territorio
+    Territorio territorio1 = {"joao","verde", 10}
+}

3fe87c2da163d9fd4cc37d54165e3c6812f16eae
Joao Victor <joaovic2012@hotmail.com>
2025-11-24 19:52:10 -0300

    funcao atacar Criada

--- war.c
+++ war.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <time.h>
 
 // Definindo a estrutura territorio
 typedef struct {
@@ -46,7 +47,39 @@ void mostrarTerritorios(Territorio *territorio, int tamanho){
      }
 }
 
+void atacar(Territorio *territorio1, Territorio *territorio2){
+    int t1 = (rand() % 6) + 1;
+    int t2 = (rand() % 6) + 1;
+
+    printf("O ataque inicia \n");
+    printf("valor do territorio %s : (%d) \n", territorio1->nome, t1);
+    printf("valor do territorio %s : (%d) \n", territorio2->nome, t2);
+
+    if(t1 > t2){
+        printf("territorio %s VENCEU", territorio1->nome);
+        //vencedor ganha tropas
+        territorio1->tropas = territorio1->tropas + territorio2->tropas;
+        //territorio muda cor
+        strcpy(territorio2->cor, territorio1->cor);
+        //territorio perde tropas
+        territorio2->tropas = 0;
+    }
+    else  if(t1 < t2){
+        printf("territorio %s VENCEU", territorio2->nome);
+        //vencedor ganha tropas
+        territorio2->tropas = territorio1->tropas + territorio2->tropas;
+        //territorio muda cor
+        strcpy(territorio1->cor, territorio2->cor);
+        //territorio perde tropas
+        territorio1->tropas = 0;
+    }
+    else if(t1 == t2){
+         printf("EMPATE!! Nada muda \n");
+    }
+}
+
 int main() {
+    srand(time(NULL));
 
     int tamanho;
 
@@ -57,6 +90,14 @@ int main() {
 
     criarTerritorio(territorio,tamanho);
     mostrarTerritorios(territorio,tamanho);
+
+    int a, b;
+    printf("\nEscolha o território que ATACA: ");
+    scanf("%d", &a);
+
+    printf("Escolha o território que será ATACADO: ");
+    scanf("%d", &b);
+    atacar(&territorio[a -1], &territorio[b - 1]);
 
     free(territorio);
 
